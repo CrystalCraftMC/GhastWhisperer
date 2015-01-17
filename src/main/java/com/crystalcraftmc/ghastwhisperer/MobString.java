a09126067895cad407221a0154a251a1fea1cc99
Ivan <Jacc734@users.noreply.github.com>
2015-01-17 02:54:48 -0500

    MobString: Even Less Code, Added Cast System
    +Fixed the problems that came with reducing the code 
    -Removed and compacted even more code so we are down to 185, as compared to 1200 lines of code

--- src/main/java/com/crystalcraftmc/ghastwhisperer/MobString.java
+++ src/main/java/com/crystalcraftmc/ghastwhisperer/MobString.java
@@ -20,11 +20,10 @@ import org.bukkit.Location;
 import org.bukkit.entity.*;
 
 import java.util.ArrayList;
+
 public class MobString {
 	//private WaterGunListener globalWgl;
-
 	private ArrayList<Entity> array1, array2, array3, array4 = new ArrayList<Entity>();
-
 	public String mobOfChoice1 = "wolf";
 	public int numberMobs1 = 2;
 	public String mobOfChoice2, mobOfChoice3, mobOfChoice4 = "no mob";
@@ -53,7 +52,6 @@ public class MobString {
 		}
 		return null;
 	}
-
 	private ArrayList<Entity> chooseArrayList(int input) {
 		switch (input) {
 		case 1:
@@ -68,55 +66,25 @@ public class MobString {
 			return null;
 		}
 	}
-
 	private EntityType chooseAnimal(String mobS) {
-		if(mobS.equalsIgnoreCase("sheep")){
+		if(mobS.equalsIgnoreCase("sheep"))
 			return EntityType.SHEEP;
-		}
-		else if(mobS.equalsIgnoreCase("wolf")){
+		else if(mobS.equalsIgnoreCase("wolf"))
 			return EntityType.WOLF;
-		}
-		else if(mobS.equalsIgnoreCase("witch")){
+		else if(mobS.equalsIgnoreCase("witch"))
 			return EntityType.WITCH;
-		}
-		else if(mobS.equalsIgnoreCase("ocelot")){
+		else if(mobS.equalsIgnoreCase("ocelot"))
 			return EntityType.OCELOT;
-		}
-		else if(mobS.equalsIgnoreCase("pig")){
+		else if(mobS.equalsIgnoreCase("pig"))
 			return EntityType.PIG;
-		}
-		else if(mobS.equalsIgnoreCase("bat")){
+		else if(mobS.equalsIgnoreCase("bat"))
 			return EntityType.BAT;
-		}
-		else if(mobS.equalsIgnoreCase("cow")){
+		else if(mobS.equalsIgnoreCase("cow"))
 			return EntityType.COW;
-		}
-		else if(mobS.equalsIgnoreCase("skeleton")){
+		else if(mobS.equalsIgnoreCase("skeleton"))
 			return EntityType.SKELETON;
-		}
 		return null;
 	}
-
-	public MobString(WaterGunListener wgl) {
-		//globalWgl = wgl;
-	}
-
-	public void cowboy(Player p, Ghast g, Location loc) {
-		this.createMobs(p, g, loc);
-		/*if(this.mobStringExists(1)) {
-
-		}
-		if(this.mobStringExists(2)) {
-
-		}
-		if(this.mobStringExists(3)) {
-
-		}
-		if(this.mobStringExists(4)) {
-
-		}*/
-	}
-
 	private boolean mobStringExists(int stringNum) {
 		boolean exists = false;
 		if(stringNum == 1) {
@@ -133,7 +101,6 @@ public class MobString {
 		}
 		return exists;
 	}
-
 	private int chooseNOM(int numberMobs) {
 		switch(numberMobs) {
 		case 1:
@@ -149,47 +116,45 @@ public class MobString {
 		}
 	}
 
-	private void createMobs(Player p, Ghast g, Location loc) {
-		this.emptyAllArrays(); //prevents unnecessary mob objects
-		//it is already ensured in ConfigureData class that no 'no mob' mobstring exists before a mob string
-		if(this.mobStringExists(1)) {
-			if (chooseMOC(1).equals(mobOfChoice1)){
-				int L = 1;
-				EntityType animal = chooseAnimal(chooseMOC(1));
+	public MobString(WaterGunListener wgl) {
+		//globalWgl = wgl;
+	}
+	public void cowboy(Player p, Ghast g, Location loc) {
+		this.createMobs(p, g, loc);
+		//TODO There was a list of if, and else if statements for mobString(1-4) to test whether they existed.
+	}
 
-				for(int i = 0; i < chooseNOM(L); i++) {
-					chooseArrayList(L).add((Entity)p.getWorld().spawnEntity(loc, animal));
-					if(i == 0)
-						g.setPassenger((Entity) chooseArrayList(L).get(i));
-					else
-						((Entity) chooseArrayList(L).get(i-1)).setPassenger((Entity) chooseArrayList(L).get(i));
-				}
-			}
-		} 
+	private void createMobs(Player p, Ghast g, Location loc) {
+		this.emptyAllArrays();
 		for (int L = 2; L < 5; L++) {
-			if (mobStringExists(L)) {
+			if(mobStringExists(L)) {
 				EntityType animal = chooseAnimal(chooseMOC(L));
 				for(int i = 0; i < chooseNOM(L); i++) {
 					chooseArrayList(L).add((Entity)p.getWorld().spawnEntity(loc, animal));
 					if(i == 0) {
-						getFinalEntity(L-1).setPassenger(chooseArrayList(L).get(i));
-					}						
-					else
-						chooseArrayList(L).get(i-1).setPassenger(chooseArrayList(L).get(i));
+						if (L == 1) g.setPassenger((Entity) chooseArrayList(L).get(i));
+						if (L >= 2 && L <= 4) getFinalEntity(L-1).setPassenger(chooseArrayList(L).get(i));
+					} 
+					else chooseArrayList(L).get(i-1).setPassenger(chooseArrayList(L).get(i));
 				}
 			}
-		}
+		} 
 		//finally.. we set the player as the passenger.
-			if(chooseNOM(1) == 0)
-				g.setPassenger(p);
-			else if(chooseNOM(2) == 0)
-				getFinalEntity(1).setPassenger(p);
-			else if(chooseNOM(3) == 0)
-				getFinalEntity(2).setPassenger(p);
-			else if(chooseNOM(4) == 0)
-				getFinalEntity(3).setPassenger(p);
-			else
-				getFinalEntity(4).setPassenger(p);
+		if(chooseNOM(1) == 0) ((Ghast) g).setPassenger((Player) p);
+		for (int R = 2; R < 6; R++) {
+			if(chooseNOM(R) == 0) {//CHECKS AND GIVES 1st, 2nd and 3rd TIER RIDES
+				int n = R-1;
+				Entity q = getFinalEntity(n);
+				if(chooseMOC(n).equalsIgnoreCase("sheep")) castSheep(q).setPassenger(p); 
+				else if(chooseMOC(n).equalsIgnoreCase("wolf")) castWolf(q).setPassenger(p);
+				else if(chooseMOC(n).equalsIgnoreCase("witch")) castWitch(q).setPassenger(p);
+				else if(chooseMOC(n).equalsIgnoreCase("ocelot")) castOcelot(q).setPassenger(p);
+				else if(chooseMOC(n).equalsIgnoreCase("pig")) castPig(q).setPassenger(p);
+				else if(chooseMOC(n).equalsIgnoreCase("bat")) castBat(q).setPassenger(p);
+				else if(chooseMOC(n).equalsIgnoreCase("cow")) castCow(q).setPassenger(p);
+				else if(chooseMOC(n).equalsIgnoreCase("skeleton")) castSkelly(q).setPassenger(p);
+			}
+		}
 	}
 
 	private void emptyAllArrays() {
@@ -201,20 +166,20 @@ public class MobString {
 
 	private Entity getFinalEntity(int whichTier) {
 		switch(whichTier) {
-		case 1:
-			return array1.get(array1.size()-1);
-
-		case 2:
-			return array2.get(array2.size()-1);
-
-		case 3:
-			return array3.get(array3.size()-1);
-
-		case 4:
-			return array4.get(array4.size()-1);
-
-		default:
-			return null;
+		case 1: return array1.get(array1.size()-1);
+		case 2: return array2.get(array2.size()-1);
+		case 3:	return array3.get(array3.size()-1);
+		case 4:	return array4.get(array4.size()-1);
+		default:return null;
 		}
 	}
+
+	private Sheep castSheep(Entity e) {return (Sheep) e;}
+	private Wolf castWolf(Entity e) {return (Wolf) e;}
+	private Witch castWitch(Entity e) {return (Witch) e;}
+	private Ocelot castOcelot(Entity e) {return (Ocelot) e;}
+	private Pig castPig(Entity e) {return (Pig)e;}
+	private Cow castCow(Entity e) {return (Cow)e;}
+	private Skeleton castSkelly(Entity e) {return (Skeleton)e;}
+	private Bat castBat(Entity e) {return (Bat)e;}
 }
